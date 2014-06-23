1430781b257ac5a14f14078be6a6ca66da6d0de5
JenGates3255 <Jengates3255@gmail.com>
2014-06-23 17:01:32 -0600

    complete

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
-var word = prompt ("Enter a word"); 
+var word = prompt("Enter a word"); 
 	alert("You entered " + word + "!");
-	alert(word + " has " + word.length + " letters")
-
-
+	alert(word + " has " + word.length + " letters");
+	alert("The third letter of your word is " + word[2]);	
+	alert("Lowercase: " + word.toLowerCase());
+	alert("Uppercase: " + word.toUpperCase());
+	alert("Example: I have wanted a " + word + " since I was little");
+	alert("Subword: " + word.substring(1,4));
 
